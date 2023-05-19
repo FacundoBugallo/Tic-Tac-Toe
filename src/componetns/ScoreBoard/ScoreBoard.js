b53519540ea30504938d3aff30f4e87687023445
Facundo <facundobugallo881@gmail.com>
2023-05-19 11:51:03 -0300

    app

---
+++ src/componetns/ScoreBoard/ScoreBoard.js
@@ -0,0 +1,10 @@
+import "./ScoreBoard.css";
+
+const ScoreBoard = (scoreO, scoreX) => {
+  <div className="score-board">
+    <div>{scoreX}</div>
+    <div>{scoreO}</div>
+  </div>
+}
+
+export default ScoreBoard;
